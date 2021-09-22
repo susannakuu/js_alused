786f1e8a828ca1c6ecc7f3e47d3fae51139094ef
susannakuu <kuusemaa.susanna@gmail.com>
2021-09-22 15:49:56 +0300

    String operations

--- app.js
+++ app.js
@@ -1,47 +1,21 @@
-// Andmetüübid
+const eesnimi = 'Susanna';
+const perenimi = 'Kuusemaa';
+const age = '25';
+const text = 'Tere, olen mina!'
+const tags = 'igast'
 
-// Primitiivid
+let val;
 
-// String - tekst
-const name = 'Anna';
-console.log(typeof name);
+//concatenation
+val = eesnimi + ' ' + perenimi;
+val = val + ' ' + age;
+val = text + 'Teen ' + tags + ' asju!';
+val = eesnimi.toUpperCase();
+val = perenimi[0];
+val = eesnimi.charAt(eesnimi.length -1);
+val = text.split(',');
 
-// Number
-const age = 15;
-console.log(typeof age);
 
-const persent = 50.3;
-console.log(typeof persent);
 
-// Boolean - true või false
-const hasKids = true;
-console.log(typeof true);
 
-// Null
-const airplane = null;
-console.log(typeof airplane);
-
-// Undefined
-let test;
-console.log(typeof test);
-
-// Symbol
-const sym = Symbol();
-console.log(typeof sym);
-
-// Objects - reference types - tüübid, mille sisu saab viide kaudu
-// Array - massiv
-const hobbies = ['movies', 'music'];
-console.log(typeof hobbies);
-
-// Object
-const person = {
-	name: 'Kadi',
-	age: 15
-};
-console.log(typeof person);
-
-// näiteks kuupäeva objekt
-const today = new Date();
-console.log(typeof today);
-console.log(today);
+console.log(val);
