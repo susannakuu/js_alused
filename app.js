dec43140354496467748d4d220d8852fb9dd0a1b
susannakuu <kuusemaa.susanna@gmail.com>
2021-09-22 15:14:59 +0300

    Types conversion

---
+++ app.js
@@ -0,0 +1,47 @@
+// Andmetüübid
+
+// Primitiivid
+
+// String - tekst
+const name = 'Anna';
+console.log(typeof name);
+
+// Number
+const age = 15;
+console.log(typeof age);
+
+const persent = 50.3;
+console.log(typeof persent);
+
+// Boolean - true või false
+const hasKids = true;
+console.log(typeof true);
+
+// Null
+const airplane = null;
+console.log(typeof airplane);
+
+// Undefined
+let test;
+console.log(typeof test);
+
+// Symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+// Objects - reference types - tüübid, mille sisu saab viide kaudu
+// Array - massiv
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+// Object
+const person = {
+	name: 'Kadi',
+	age: 15
+};
+console.log(typeof person);
+
+// näiteks kuupäeva objekt
+const today = new Date();
+console.log(typeof today);
+console.log(today);
