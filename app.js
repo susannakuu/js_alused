188d3e9d269ebbd1a0c6f9ef1ca696141e8de7ed
susannakuu <kuusemaa.susanna@gmail.com>
2021-09-27 11:47:08 +0300

    Condition statement switch+case

--- app.js
+++ app.js
@@ -1,68 +1,44 @@
-/*
-if(tingimus) {
-	// tegevused if puhul
-} else {
-	// tegevused else puhul
-}
+/*const color = "red";
 
-*/
-const id = 100;
-/*
-// == - võrdub
-// != - ei võrdu
-// === - võrdub ja tüübikontroll
-// !== - ei võrdu ja tüübikontroll
-
-let testId;
-
-if(typeof testId !== 'undefined'){
-	console.log(`id on ${testId}`);
-} else {
-	console.log('id puudub');
-}
-*/
-/*
-
-if(id > 100) {
-	console.log(`id on ${id} - suurem kui 100`);
-
-} else {
-	console.log(`id on ${id} - väiksem või võrdne 100`);
-}
-*/
-/*
-const color = "red";
-
-if(color=== 'red') {
-	console.log('Stop!');
-} else if(color === 'yellow'){
-	console.log('Attention');
-} else (color === "green"){
-	console.log('Go!')
+switch(color) {
+	case 'red':
+		console.log('stop!');
+		break;
+	case 'yellow':
+		console.log('attention!');
+		break;
+	case 'green':
+		console.log('Go!');
+		break;
 }
 */
 
-const name = 'Kadi';
-const age = 15;
-if(age > 0 && age <=22){
-	console.log(`${name} on laps`);
-} else if(age > 12 && age < 18){
-	console.log(`${name} on nooruk`);
-} else {
-	console.log(`${name} on täiskasvanud`);
+
+let day = new Date('10/03/2021');
+let dayNumber = day.getDay();
+console.log(dayNumber);
+
+
+switch(dayNumber) {
+	case 0:
+		console.log('Pühapäev');
+		break;
+	case 1:
+		console.log('Esmaspäev');
+		break;
+	case 2:
+		console.log('Teisipäev');
+		break;
+	case 3:
+		console.log('Komapäev');
+		break;
+	case 4:
+		console.log('Neljapäev');
+		break;
+	case 5:
+		console.log('Reede');
+		break;
+	case 6:
+		console.log('Laupäev');
+		break;
 }
-
-if(age < 18 || age > 65){
-	console.log(`${name} ei saa registreeruda`);
-} else {
-	console.log(`${name} on registreeritud`);
-}
-
-// ? - ternary operaator
-console.log(id === 100 ? 'õige' : 'vale');
-
-// ilma {}
-if(id === 100)
-	console.log('korras');
-else
-	console.log('ei ole korras');
