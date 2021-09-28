e01023699e49150a40b5a02bb9b60d9a7fa826e3
susannakuu <kuusemaa.susanna@gmail.com>
2021-09-28 09:50:54 +0300

    Functions

--- app.js
+++ app.js
@@ -1,66 +1,50 @@
-// for tsükkel
-console.log('for tsükkel');
-for(let i = 0; i < 10; i++){
-	if(i == 5){
-		//break;
-		continue;
+// funktsiooni defineerimine (deklareerimine)
+function greeting(firstname = 'Kadi', lastname = 'Tamm'){
+	/*if(typeof firstname === 'undefined'){
+		firstname ='Kadi';
 	}
-	console.log(i);
+	if(typeof lastname === 'undefined'){
+		lastname ='Tamm';
+	}*/
+	return 'Tere, ' + firstname + ' ' + lastname;
 }
+//funktsiooni tööle kutsumine
 
-// while tsükkel
-console.log('while tsükkel');
-
-let i = 0;
-while(i<10){
-	console.log(i);
-	i++;
-}
-
-// do while
-console.log('do while tsükkel');
-
-let j = 0;
-do {
-	console.log(j);
-	j++;
-} while(j < 10);
-
-
-// tsüklid ja massiivid
-console.log('massiivid ja tsüklid');
-const cars = ['Ford', 'Honda', 'Ferrari'];
-
-// for tsükkliga 
-for(let i = 0; i < cars.length; i++){
-	console.log(cars[i]);
-}
+let greet = greeting('Susanna', 'Kuu');
+console.log(greet);
 
 console.log();
-
-// forÉach
-cars.forEach(function(element, index, array){
-	console.log(`cars[${index}] = ${element}`);
-	console.log(array);
-});
-
-
-// forÉach - (arrow) => function
-cars.forEach((element, index) => {
-	console.log(`cars[${index}] = ${element}`);
-});
-
-
-
-
-// tsüklid ja objektid
-const person = {
-	firstname: 'Kadi',
-	surname: 'Tamm',
-	age: 25
+// funktsioon avaldise kujul
+const square = function(number = 3){
+	return number * number;
 }
 
-// for in tsükkel
-for(let key in person) {   //key = value
-	console.log(`${key} = ${person[key]}`)
-}     
+console.log(square());
+
+
+// IIFE Immediately Invokable Function Expression
+(function(){
+	console.log('function ran');
+})();
+
+(function(name){
+	console.log('Tere ' + name);
+})('Susanna');
+
+console.log();
+// objekti siseselt defineeritud funktsioon
+const todo = {
+	add: function(){
+		console.log('add todo nr...');
+	},
+	edit: function(id){
+		console.log(`edit todo nr ${id}...`);
+	},
+	delete: function(){
+		console.log('delete todo...');
+	}
+}
+
+todo.add();
+todo.edit(5);
+todo.delete();
