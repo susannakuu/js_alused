b6fb00ddbab49cb525829f39f6f6b9c465715e0a
susannakuu <kuusemaa.susanna@gmail.com>
2021-09-28 09:17:48 +0300

    Tsüklid

--- app.js
+++ app.js
@@ -1,44 +1,66 @@
-/*const color = "red";
-
-switch(color) {
-	case 'red':
-		console.log('stop!');
-		break;
-	case 'yellow':
-		console.log('attention!');
-		break;
-	case 'green':
-		console.log('Go!');
-		break;
+// for tsükkel
+console.log('for tsükkel');
+for(let i = 0; i < 10; i++){
+	if(i == 5){
+		//break;
+		continue;
+	}
+	console.log(i);
 }
-*/
 
+// while tsükkel
+console.log('while tsükkel');
 
-let day = new Date('10/03/2021');
-let dayNumber = day.getDay();
-console.log(dayNumber);
-
-
-switch(dayNumber) {
-	case 0:
-		console.log('Pühapäev');
-		break;
-	case 1:
-		console.log('Esmaspäev');
-		break;
-	case 2:
-		console.log('Teisipäev');
-		break;
-	case 3:
-		console.log('Komapäev');
-		break;
-	case 4:
-		console.log('Neljapäev');
-		break;
-	case 5:
-		console.log('Reede');
-		break;
-	case 6:
-		console.log('Laupäev');
-		break;
+let i = 0;
+while(i<10){
+	console.log(i);
+	i++;
 }
+
+// do while
+console.log('do while tsükkel');
+
+let j = 0;
+do {
+	console.log(j);
+	j++;
+} while(j < 10);
+
+
+// tsüklid ja massiivid
+console.log('massiivid ja tsüklid');
+const cars = ['Ford', 'Honda', 'Ferrari'];
+
+// for tsükkliga 
+for(let i = 0; i < cars.length; i++){
+	console.log(cars[i]);
+}
+
+console.log();
+
+// forÉach
+cars.forEach(function(element, index, array){
+	console.log(`cars[${index}] = ${element}`);
+	console.log(array);
+});
+
+
+// forÉach - (arrow) => function
+cars.forEach((element, index) => {
+	console.log(`cars[${index}] = ${element}`);
+});
+
+
+
+
+// tsüklid ja objektid
+const person = {
+	firstname: 'Kadi',
+	surname: 'Tamm',
+	age: 25
+}
+
+// for in tsükkel
+for(let key in person) {   //key = value
+	console.log(`${key} = ${person[key]}`)
+}     
