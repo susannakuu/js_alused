1ccfa410a676ed8ecb5e5fd268fe6e9585b82aa6
susannakuu <kuusemaa.susanna@gmail.com>
2021-09-28 11:47:54 +0300

    Document.getElement

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-let val;
+let val
+//document.getElementById();
 
-val = document;
-val = document.all;
-val = document.all[5]
-val = document.all.length;
+val = document.getElementById("task-title");
+val = document.getElementById("task-title").id;
+val = document.getElementById("task-title").className;
 
+const taskTitle = document.getElementById("task-title");
 
-val = document.head;
-val = document.body;
-
-val = document.doctype;
-
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
-
-val = document.forms;
-val = document.forms[0];
-val = document.forms.method;
-val = document.forms.action;
-val = document.forms[0].id;
+taskTitle.style.color = "purple";
+//taskTitle.style.display = "none";   //võtab maha applied stilingud
 
 
-console.log(val);
+// sisu muutmine
+taskTitle.textContent = "My Tasks";
+taskTitle.innerText = "Yada-Yada";
+//taskTitle.innerHTML = '<span style="color: red;">My Tasks</span>'
+
+//
+
+val = document.querySelector('#task-title');
+val = document.querySelector('.card-title');
+
+document.querySelector('li').style.color = 'pink';
+document.querySelector('li:last-child').style.color = 'fuchsia';
+document.querySelector('li:nth-child(odd)').style.background = '#ffff';
+
+
+
+console.log();
