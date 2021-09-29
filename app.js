cc39f354af9d1e230e8eda883ffc2e04400ed8f7
susannakuu <kuusemaa.susanna@gmail.com>
2021-09-29 15:35:05 +0300

    DOM elemendi loomine

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
-let val;
+
+//elemendi loomine
+const li = document.createElement('li');
 
 const list = document.querySelector('ul');
-const listItem = document.querySelector('li:first-child');
 
-val = list;
-val = listItem;
+//klassi määramine
+li.className = 'collection-item';
 
-val = list.childNodes;
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName;
-val = list.childNodes[0].nodeType;
+//teksti lisamine
+li.appendChild(document.createTextNode('Study element creation'));
 
-val = list.children;
-val = list.children[1];
+const link = document.createElement('a');
+link.className = 'secondary-content';
+link.appendChild(document.createTextNode('x'));
 
-list.children[1].textContent = 'Study XML';
+//lisa atribuut väärtusega
+link.setAttribute('href', '#');
+li.appendChild(link);
 
-val = list.children[2].children;
-list.children[2].children[0].id = 'test-id';
 
-val = list.firstChild;
-val = list.firstElementChild;
+//elemendi lisamine teise elemendi sisse
+list.appendChild(li);
 
-val = list.lastChild;
-val = list.lastElementChild;
 
-val = list.childElementCount;
-
-val = listItem.parentNode;
-val = listItem.parentElement;
-val = listItem.parentElement.parentElement;
-
-val = listItem.nextSibling;
-val = listItem.nextElementSibling;
-val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
-
-console.log(val);
+console.log(li);
