cf6f82a7ce94a8a7062324e0bab8f12faca98a10
susannakuu <kuusemaa.susanna@gmail.com>
2021-09-29 15:15:19 +0300

    Dom - navigeerimine

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-let val
+let val;
 
-val = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+const listItem = document.querySelector('li:first-child');
 
-const oddLi = document.querySelectorAll('li:nth-child(odd)');
+val = list;
+val = listItem;
 
-const evenLi = document.querySelectorAll('li:nth-child(even)');
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[0].nodeType;
 
-evenLi.forEach(function(li){
-	li.style.background = '#4073FF';
-});
+val = list.children;
+val = list.children[1];
 
-oddLi.forEach(function(li){
-	li.style.background = '#96C3EB';
-});
+list.children[1].textContent = 'Study XML';
 
-console.log(even);
+val = list.children[2].children;
+list.children[2].children[0].id = 'test-id';
+
+val = list.firstChild;
+val = list.firstElementChild;
+
+val = list.lastChild;
+val = list.lastElementChild;
+
+val = list.childElementCount;
+
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+val = listItem.nextSibling;
+val = listItem.nextElementSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+console.log(val);
