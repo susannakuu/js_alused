dff65d70d2df06bb001fde347e521c6ff6a1e424
susannakuu <kuusemaa.susanna@gmail.com>
2021-10-04 11:35:46 +0300

    Ülesanne taski adddimiseks

--- app.js
+++ app.js
@@ -30,4 +30,24 @@ function runEvent(e) {
 	heading.innerText = e.target.value;
 	//e.preventDefault();
 }
+form.addEventListener('submit', addTask);
 
+function addTask(e) {
+	const li = document.createElement('li');
+	const list = document.querySelector('ul');
+
+	li.className = 'collection-item';
+	li.appendChild(document.createTextNode(taskInput.value));
+
+	const link = document.createElement('a');
+	link.className = 'secondary-content';
+	link.appendChild(document.createTextNode('x'));
+	link.setAttribute('href', '#');
+
+	li.appendChild(link);
+	list.appendChild(li);
+
+	taskInput.value ='';
+
+	e.preventDefault();
+}
